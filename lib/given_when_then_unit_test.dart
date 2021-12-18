5ce9e2fb4e47327a80b033a6d8097d87ebce30ed
Andrew Piterov <piterov1990@gmail.com>
2021-12-18 13:56:08 +0200

    [Add] test cases

--- lib/given_when_then_unit_test.dart
+++ lib/given_when_then_unit_test.dart
@@ -1,6 +1,7 @@
 library given_when_then_unit_test;
 
 export 'res/given.dart';
+export 'res/test_case.dart';
 export 'res/test_fixtures.dart';
 export 'res/then.dart';
 export 'res/when.dart';
---
+++ test/test_case_test.dart
@@ -0,0 +1,21 @@
+import 'package:given_when_then_unit_test/given_when_then_unit_test.dart';
+import 'package:shouldly/shouldly.dart';
+
+void main() {
+  testCases([
+    const TestCase([1, 1, 2]),
+    const TestCase([5, 3, 8])
+  ], (testCase) {
+    final x = testCase.args[0] as int;
+    final y = testCase.args[1] as int;
+
+    given('two numbers $x and $y', () {
+      //
+      when('summarizing them', () {
+        then('the result should be equal to ${testCase.args.last}', () {
+          (x + y).should.be(testCase.args[2] as int);
+        });
+      });
+    });
+  });
+}
